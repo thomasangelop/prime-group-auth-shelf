e0663bfdec95f34109d3e8c18cd48c5584e564da
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2018-11-15 13:32:31 -0600

    added view reducer file, added view component, updated get router

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,15 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+	const sqlText = 'SELECT * FROM item ORDER BY id DESC;';
+	pool.query(sqlText)
+		.then((result) => {
+			res.send(result.rows);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.sendStatus(500);
+		})
 });
 
 
